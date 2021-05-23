90ae0a0832e101c7df50ffd10221cdf8e81b9b62
Phi Truong Nguyen <phitruong.nd2k@gmal.com>
2021-05-23 19:10:44 +0700

    user page

--- src/pub/js/component/user-item.js
+++ src/pub/js/component/user-item.js
@@ -3,21 +3,19 @@ fetch('/api/getAllUsers')
     .then(users => {
         const htmls = users.map(user => {
             return '<div class="user-item">'+
-            '        <div class="wrapper2">'+
-            '            <div class="profile">'+
-            `                <img src="${user.image}" class="thumbnail" onerror=\'this.src="/img/icon/avatar-default.png"\'>`+
-            '                <div class="check"><i class="fas fa-check"></i></div>'+
-            `                <h3 class="name">${user.name}</h3>`+
-            `                <p class="title">${user.email}</p>`+
-            `                <a href="/app/user?userId=${user._id}"><button type="button" class="btn">See More</button></a>`+
-            '            </div>'+
-            '        <div class="user-info">'+
-            `            <p>Job: ${user.job ? user.job : '---'}</p>`+
-            `            <p>Country: ${user.city + ', ' + user.country}</p>`+
-            `            <p><i>"${user.description ? user.description : 'Let\'s see my page.'}"</i></p>`+
-            '        </div>'+
-            '        </div>'+
-            '    </div>';
+            '                <div class="our-team">'+
+            '                    <div class="picture">'+
+            `                        <img class="img-fluid" src="${user.image}" onerror=\'this.src="/img/icon/avatar-default.png"\'>`+
+            '                    </div>'+
+            '                    <div class="team-content">'+
+            `                        <h3 class="name">${user.name}</h3>`+
+            `                        <h4 class="title">${user.job}</h4>`+
+            '                    </div>'+
+            '                    <ul class="social">'+
+            `                        <li><a href="/app/user?userId=${user._id}" aria-hidden="true">View more</a></li>`+
+            '                    </ul>'+
+            '                </div>'+
+            '            </div>';
         })
 
         document.getElementById('user-list').innerHTML = htmls.join('');
